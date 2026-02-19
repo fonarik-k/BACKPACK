a73310a1ea63bf655cad8d0ebd682ef0c5690df8
fonarik-k <prosto87879@gmail.com>
2026-02-19 20:52:53 +0500

    sale section

--- app.js
+++ app.js
@@ -25,3 +25,33 @@ products.forEach((product) => {
       </div>
     `)
   })
+
+  
+  document.addEventListener("DOMContentLoaded", () => {
+
+    const targetDate = new Date("February 24, 2026 14:45:45");
+    const timerEl = document.querySelector(".timer");
+
+    function updateTimer() {
+        const now = new Date();
+        const diff = targetDate - now;
+
+        if (diff <= 0) {
+            timerEl.textContent = "Time is over!";
+            clearInterval(interval);
+            return;
+        }
+
+        const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+        const minutes = Math.floor((diff / (1000 * 60)) % 60);
+        const seconds = Math.floor((diff / 1000) % 60);
+
+        timerEl.textContent =
+            `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    }
+
+    updateTimer(); // сразу показать
+    const interval = setInterval(updateTimer, 1000);
+
+});
